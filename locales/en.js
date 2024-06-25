95bd1460404943e015843d57eedd36cd7787ad8e
KudesnikRaf <kolesnik.akk@gmail.com>
2024-06-25 15:46:52 +0300

    add locales

---
+++ locales/en.js
@@ -0,0 +1,22 @@
+export default {
+  translation: {
+    heading: 'RSS Reader',
+    subheading: "Start reading RSS today! It's simple and lovely.",
+    RSSLink: 'RSS link',
+    example: 'An example: https://hexlet.io/lessons.rss',
+    readAllBtn: 'Read more',
+    modalBtnClose: 'Close',
+    successfullyLoaded: 'RSS has been loaded',
+    addBtn: 'Add',
+    posts: 'Posts',
+    feeds: 'Feeds',
+    preview: 'Preview',
+    errors: {
+      required: 'This is a required field',
+      invalid: 'Must be valid URL',
+      alreadyExists: 'RSS already exists',
+      noData: 'No valid RSS in the resource',
+      'Failed to fetch': 'Network error',
+    },
+  },
+};
---
+++ locales/ru.js
@@ -0,0 +1,23 @@
+export default {
+  translation: {
+    heading: 'RSS агрегатор',
+    subheading: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    RSSLink: 'Ссылка RSS',
+    add: 'Добавить',
+    example: 'Пример: https://lorem-rss.hexlet.app/feed',
+    readAllBtn: 'Читать полностью',
+    modalBtnClose: 'Закрыть',
+    addBtn: 'Добавить',
+    successfullyLoaded: 'RSS успешно загружен',
+    posts: 'Посты',
+    feeds: 'Фиды',
+    preview: 'Просмотр',
+    errors: {
+      required: 'Это обязательное поле',
+      invalid: 'Ссылка должна быть валидным URL',
+      alreadyExists: 'RSS уже существует',
+      noData: 'Ресурс не содержит валидный RSS',
+      'Failed to fetch': 'Ошибка сети',
+    },
+  },
+};
